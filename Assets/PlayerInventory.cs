8b1ce9d559aaa8574dec4e751b32952dc836ff6b
sama oruj <170125792+sema0rucova@users.noreply.github.com>
2025-05-18 06:06:32 +0300

    commit

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public bool isCarryingAxolotl = false;
+    public Transform efektPozisyonu;
+    public GameObject kalpEfektiPrefab;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Axolotl") && !isCarryingAxolotl)
         {
             isCarryingAxolotl = true;
-            Destroy(other.gameObject); // Ya da gizle
-            // Belki karakterin üstünde bir sprite ile göster
+            Destroy(other.gameObject);
         }
 
         if (other.CompareTag("Lake") && isCarryingAxolotl)
         {
             isCarryingAxolotl = false;
-            FindObjectOfType<AxolotlManager>().RescueAxolotl();
+
+            // Kalp efekti instantiate edilir
+            Instantiate(kalpEfektiPrefab, efektPozisyonu.position, Quaternion.identity);
         }
     }
 }
