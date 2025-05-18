78eb936715f3bb8c4c23ac705e59fff3d113eaef
Metehan <gliencherth@gmail.com>
2025-05-18 09:45:26 +0300

    Eden onprogress

--- Assets/Scenes/Act-2/Act-2SP/StatueManager.cs
+++ Assets/Scenes/Act-2/Act-2SP/StatueManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class StatueManager : MonoBehaviour
 {
-    public static int axolotlCount = 7; // Static variable to keep track of the number of axolotls saved.
+    // Static variable to keep track of the number of axolotls saved.
     public GameObject[] statues; // Array of statue GameObjects.
 
     public void Start()
     {
+        int axolotlCount=PlayerPrefs.GetInt("totalRescuedAxolotls", 7); // Get the number of axolotls saved from PlayerPrefs.
         for (int i=0; i<(axolotlCount>6?4:axolotlCount>5?3:axolotlCount>4?2:axolotlCount>3?1:0); i++)
         {
             statues[i].SetActive(true); // Activate all statues.
